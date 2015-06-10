858170baed947660b9e9ad714d81cb86fb201fa0
Tomasz Dysinski <tom@jam3.com>
2015-06-10 10:05:43 -0400

    easing, code cleanup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ---- Keyboard
 var Keyboard = require('game-keyboard');
 var keyMap = require('game-keyboard/key_map')['US'];
-var keyboard = new Keyboard(keyMap);
+var clamp = require('clamp');
 
 // ---- dat-gui
 var dat = require('dat-gui');
@@ -39,67 +39,61 @@ function getKeyValue(keyStr)
 
 function DatGuiKeyboardHelper()
 {
-	// --------------- GUI
 	this.gui = new dat.GUI();
+	this.keyboard = new Keyboard(keyMap);
 	this.buttons = [];
 	this.sliders = [];
 
-	this.hookItUp();
-}
-
-DatGuiKeyboardHelper.prototype.hookItUp = function() {
-
 	// Starts the keyboard loop
-	setInterval(function() {
-
-		for (var i = 0; i < this.buttons.length; i++)
-		{
-			var button = this.buttons[i];
-
-			if (keyboard.consumePressed(button.keyValue))
-			{
-				button.object[button.valueKey]();
-			}
-		}
-
-		for (var i = 0; i < this.sliders.length; i++)
-		{
-			var slider = this.sliders[i];
-
-			if (keyboard.isPressed(slider.increaseKeyValue))
-			{
-				if (slider.object[slider.valueKey] + slider.increment <= slider.max)
-				{
-					slider.object[slider.valueKey] =
-					slider.object[slider.valueKey] + slider.increment;	
-
-					slider.onChangeCallback();
-				}
-			}
-
-			if (keyboard.isPressed(slider.decreaseKeyValue))
-			{
-				if (slider.object[slider.valueKey] - slider.increment >= slider.min)
-				{
-					slider.object[slider.valueKey] =
-					slider.object[slider.valueKey] - slider.increment;	
-
-					slider.onChangeCallback();
-				}
-			}
-		}
-
-	}.bind(this), 1000 / 60);
+	this.keyboardLoop = this.keyboardLoop.bind(this);
+	setInterval(this.keyboardLoop, 1000 / 60);
 };
 
-DatGuiKeyboardHelper.prototype.addSlider = function(
-	object, valueKey, onChangeCallback, 
-	increaseKeyStr, decreaseKeyStr, increment, label, min, max )
-{
+DatGuiKeyboardHelper.prototype.keyboardLoop = function() {
+	for (var i = 0; i < this.buttons.length; i++)
+	{
+		var button = this.buttons[i];
+
+		if (this.keyboard.consumePressed(button.keyValue))
+		{
+			button.object[button.valueKey]();
+		}
+	}
+
+	for (var i = 0; i < this.sliders.length; i++)
+	{
+		var slider = this.sliders[i];
+		var object = slider.object;
+		var valueKey = slider.valueKey;
+		var used = false;
+		var oldValue = object[valueKey];
+
+		var direction = 0;
+		if(this.keyboard.isPressed(slider.increaseKeyValue)) direction += 1;
+		if(this.keyboard.isPressed(slider.decreaseKeyValue)) direction -= 1;
+
+		if (direction != 0) {
+			object[valueKey] += slider.increment * slider.ease * slider.ease * direction;
+			if(slider.ease < 2) slider.ease += 0.1;
+			object[valueKey] = clamp(object[valueKey], slider.min, slider.max);
+			if(object[valueKey] !== oldValue) slider.onChangeCallback();
+		} else {
+			slider.ease = 0.1;
+		}
+	}
+}
+
+DatGuiKeyboardHelper.prototype.addSlider = function( object, valueKey, onChangeCallback, increaseKeyStr, decreaseKeyStr, increment, label, min, max) {
 	label = '│' + increaseKeyStr + '║' + decreaseKeyStr + '│ ' + label;
 
 	var increaseKeyValue = getKeyValue(increaseKeyStr);
 	var decreaseKeyValue = getKeyValue(decreaseKeyStr);
+
+	this.sliders.forEach(function(slider) {
+		if(slider.increaseKeyValue === increaseKeyValue || slider.decreaseKeyValue === decreaseKeyValue) {
+			console.warn('You\'ve assigned the same keyboard key to multiple controls.');
+		}
+	})
 
 	var widget = this.gui.add(object, valueKey, min, max).name(label).listen();
 	widget.onChange(onChangeCallback);
@@ -108,6 +102,7 @@ DatGuiKeyboardHelper.prototype.addSlider = function(
 		increaseKeyValue: increaseKeyValue,
 		decreaseKeyValue: decreaseKeyValue,
 		increment: increment,
+		ease: 0,
 		min: min,
 		max: max,
 		object: object,
@@ -116,9 +111,7 @@ DatGuiKeyboardHelper.prototype.addSlider = function(
 	});
 };
 
-DatGuiKeyboardHelper.prototype.addButton = function(
-	object, valueKey, keyStr, label	)
-{
+DatGuiKeyboardHelper.prototype.addButton = function(object, valueKey, keyStr, label) {
 	label = '│' + keyStr + '│ ' + label;
 
 	var keyValue = getKeyValue(keyStr);
